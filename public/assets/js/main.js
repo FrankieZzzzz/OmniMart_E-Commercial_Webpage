a8980561a782cfeceda06b6546e7347187f47b49
FrankieZzzz <zhongweini0703@gmail.com>
2024-03-09 17:51:42 -0500

    Mar 9
    finish header design, upload logo and finish login and search page

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -14,5 +14,22 @@ if (searchClose) {
     searchClose.addEventListener('click', () =>{
         searchContent.classList.remove('show-search')
     })
-    
+}
+
+/*=============== Login ===============*/
+let loginBtn = document.getElementById('login-button'),
+    loginClose = document.getElementById('login-close'),
+    loginContent = document.getElementById('login-content');
+
+/*=============== login show ===============*/
+if (loginBtn) {
+    loginBtn.addEventListener('click',  () =>{
+        loginContent.classList.add('show-login');
+    })
+}
+/*=============== login hidden ===============*/
+if (loginClose) {
+    loginClose.addEventListener('click', () =>{
+        loginContent.classList.remove('show-login')
+    })
 }
