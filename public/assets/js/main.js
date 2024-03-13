6f86409b23024471062d8fb2d729db14ab201f58
FrankieZzzz <zhongweini0703@gmail.com>
2024-03-12 23:43:05 -0400

    Mar 12
    finish all sections and responsible design, test dark mode. edit dark mode css

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -45,7 +45,7 @@ new Swiper('#home__swiper', {
       coverflowEffect: {
         rotate: 50,
         stretch: 0,
-        depth: 100,
+        depth: 200,
         modifier: 1,
         slideShadows: true,
       },
@@ -68,6 +68,15 @@ new Swiper('#featured__swiper', {
         el: ".swiper-pagination",
         clickable: true,
     },
+    breakpoints: {
+        576: {
+            slidesPerView:2
+        },
+        1150: {
+            slidesPerView:4,
+            centeredSlides:false,
+        }
+    }
 });
 
 /*=============== new arrival swiper ===============*/
@@ -77,6 +86,14 @@ new Swiper('#new__swiper', {
     el: ".swiper-pagination",
     dynamicBullets: true,
     },
+    breakpoints: {
+        576: {
+            slidesPerView:2
+        },
+        1150: {
+            slidesPerView:4,
+        }
+    }
 });
 
 /*=============== testimonial swiper Autoplay===============*/
@@ -92,6 +109,15 @@ new Swiper("#testimonial__swiper", {
             el: ".swiper-pagination",
             clickable: true,
         },
+        breakpoints: {
+        576: {
+            slidesPerView:2
+        },
+        1150: {
+            slidesPerView:4,
+            centeredSlides:false,
+        }
+    }
     });
 
 
@@ -102,3 +128,70 @@ const shadowHeader = ()=>{
     : header.classList.remove('shadow-header')
 }
 window.addEventListener('scroll', shadowHeader)
+
+/*=============== show scroll up===============*/
+let scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll'):scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== active linke show ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*='+ sectionId + ']')
+
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }else{
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive);
+
+/*=============== dark theme ===============*/
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'fa-snowflake';
+// // previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// // we obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'fa-moon' : 'fa-snowflake';
+
+// // if user choose a theme
+if(selectedTheme){
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'fa-moon' ? 'add' : 'remove'](iconTheme)
+}
+// // Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+/*=============== scroll reveal js ===============*/
+const sr = ScrollReveal({
+    origin:'top',
+    distance:'60px',
+    duration:2500,
+    delay:400,
+    // reset: true,
+    // Animations repeat
+})
+sr.reveal(`.home__data, .featured__container, .new__container, .join__data, .testimonial__container, .footer`)
+sr.reveal(`.home__images`, {delay:200})
+sr.reveal(`.services__card`, {interval:50})
+sr.reveal(`.discount__data`, {origin:'left'})
+sr.reveal(`.discount__images`, {origin:'right'})
